d2f9bbfb539e7f419fbb74e2837d3ce306060ee4
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-03-18 17:58:33 +0000

    Fix

--- src/minift.c
+++ src/minift.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:39:56 by otodd             #+#    #+#             */
-/*   Updated: 2024/03/18 17:41:34 by otodd            ###   ########.fr       */
+/*   Updated: 2024/03/18 17:58:26 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	ft_atoi(const char *nptr)
 	c = (char *)nptr;
 	operation = 1;
 	value = 0;
-	while ((c == ' ') || (c == '\t')
-		|| (c == '\v') || (c == '\r') || (c == '\n') || (c == '\f'))
+	while ((*c == ' ') || (*c == '\t')
+		|| (*c == '\v') || (*c == '\r') || (*c == '\n') || (*c == '\f'))
 		c++;
 	if (*c == '+' || *c == '-')
 	{
@@ -28,7 +28,7 @@ int	ft_atoi(const char *nptr)
 			operation *= -1;
 		c++;
 	}
-	while (c >= 48 && c <= 57)
+	while (*c >= 48 && *c <= 57)
 		value = (value * 10) + (*c++ - '0');
 	return (value * operation);
 }
