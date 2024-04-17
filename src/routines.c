75a201aa450bf25f52d40e62d4a3f6cb7c48636e
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-04-17 13:52:43 +0100

    Update

--- src/routines.c
+++ src/routines.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:13:55 by otodd             #+#    #+#             */
-/*   Updated: 2024/04/15 18:32:50 by otodd            ###   ########.fr       */
+/*   Updated: 2024/04/17 13:41:04 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ void	*life(void *i)
 	carbon = (t_carbon *)i;
 	carbon->is_ready = true;
 	while (get_total_soul_ready_count(carbon->earth) != carbon->earth->nop)
-		usleep(10);
-	if (carbon->id % 2 && carbon->earth->nop > 1)
-		slumber(carbon->earth->tte / 50, carbon);
+		usleep(150);
 	while (!carbon->earth->solar_flare && carbon->state != DEAD)
 	{
 		eating(carbon);
